85ae7b4efef3f9ac094af9db4336b0e3b71548bd
SpatiallyAbsent <joshuapdow@gmail.com>
2024-03-16 13:42:43 -0600

    added git, linked-in, and email to FootIcons.jsx and connected it to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 //bellow is where we importated our navtabs We need to make a footer similar to this
 import Nav from './components/NavTabs';
+import FootIcons from './components/FootIcons';
 // used example from 20-React/23-Ins_React-Router
 function App() {
     return (
@@ -9,6 +10,7 @@ function App() {
         <main>
           <Outlet />
         </main>
+        <FootIcons />
       </>
     );
   }
--- src/components/FootIcons.jsx
+++ src/components/FootIcons.jsx
@@ -1,6 +1,26 @@
 import { Link, useLocation } from "react-router-dom";
+// used npm install react-icons and imported the ones i needed below
+import { FaGithub, FaLinkedin, FaEnvelope } from "react-icons/fa";
 // add logic here for icons and footer
 // need icons that link to github, linkedin, and email
+ function FootIcons() {
+    const location = useLocation();
+    return (
+        <footer className="footer">
+            <div className="social-icons">
+                <a href="https://github.com/spatiallyabsent" target="_blank" rel="noopener noreferrer">
+                    <FaGithub />
+                </a>
+                <a href="https://www.linkedin.com/in/josh-dow-7985112b8/" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin />
+                </a>
+                <a href="mailto:joshuapdow@gmail.com">
+                    <FaEnvelope />
+                </a>
+            </div>
+        </footer>
+    );
+}
 
 
 export default FootIcons;
