748f64e93c2551ed607273a6205f2b840f9d4aa8
SpatiallyAbsent <joshuapdow@gmail.com>
2024-03-16 12:26:45 -0600

    added component to swap between different pages in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,15 @@
+import { Outlet } from 'react-router-dom';
+import Nav from './components/Nav';
+// used example from 20-React/23-Ins_React-Router
+function App() {
+    return (
+      <>
+        <Nav />
+        <main>
+          <Outlet />
+        </main>
+      </>
+    );
+  }
 
+  export default App;
