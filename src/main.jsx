5f0eb2d248575277912d10b0ae96656aa589bcf2
SpatiallyAbsent <joshuapdow@gmail.com>
2024-03-16 12:22:19 -0600

    added basic routing in main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from 'react' //may not need this
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import './index.css'
 
-import App from './App.jsx'
+import App from './App';
+import About from './pages/About';
+import Contact from './pages/Contact';
+import Error from './pages/Error';
+import Home from './pages/Home';
+import Portfolio from './pages/Portfolio';
+import Resume from './pages/Resume';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
+
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
