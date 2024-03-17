c41a4c8c83dc5aeea15d77e6d6e6a5bba3a7541a
SpatiallyAbsent <joshuapdow@gmail.com>
2024-03-17 15:41:42 -0600

    added html for Resume.jsx

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -2,9 +2,31 @@ export default function Resume() {
     return (
         <div>
             <h1>Resume</h1>
-            <p>
-                Place Holder
-            </p>
+            <h2>Front End Technologies</h2>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JavaScript</li>
+                <li>jQuery</li>
+                <li>React</li>
+                <li>Bootstrap</li>
+                <li>Handlebars</li>
+            </ul>
+            <h2>Back End Technologies</h2>
+            <ul>
+                <li>Node.js</li>
+                <li>Express.js</li>
+                <li>MySQL</li>
+                <li>MongoDB</li>
+            </ul>
+            <h2>Other</h2>
+            <ul>
+                <li>GitHub</li>
+                <li>Heroku</li>
+                <li>VS Code</li>
+                <li>Insomnia</li>
+                <li>Render</li>
+            </ul>
         </div>
     );
 }
